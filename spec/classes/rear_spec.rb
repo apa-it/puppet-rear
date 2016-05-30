c531d5497c7c46812d4ce0fa651d9c26c9a88391
thbe <project@bendler-net.de>
2016-05-30 15:34:28 +0200

    Fixed spec file

--- spec/classes/rear_spec.rb
+++ spec/classes/rear_spec.rb
@@ -1,13 +1,4 @@
 require 'spec_helper'
-describe 'rear' do
-
-  context 'with default values for all parameters' do
-    it { should contain_class('rear') }
-  end
-end
-
-
-require 'spec_helper'
 
 describe 'rear', :type => :class do
 
@@ -32,43 +23,31 @@ describe 'rear', :type => :class do
 
       it { is_expected.to compile.with_all_deps }
 
-      it { is_expected.to contain_class('rear') }
-      it { is_expected.to contain_class('rear::params') }
-      it { is_expected.to contain_class('rear::package') }
-      it { is_expected.to contain_class('rear::config') }
-      it { is_expected.to contain_class('rear::service') }
-
-      it { is_expected.to contain_package('rear').with_ensure('installed') }
-
-      it { is_expected.to contain_file('/etc/rear/local.conf').with_ensure('file') }
-
-      it { is_expected.to contain_file('/etc/cron.d/rear').with_ensure('file') }
-
-      it 'should generate valid content for configuration file local.config' do
-        content = catalogue.resource('file', '/etc/rear/local.conf').send(:parameters)[:content]
-        expect(content).to match('OUTPUT=ISO')
-        expect(content).to match('OUTPUT_URL=nfs://192.168.1.20/srv/rear/domain')
-        expect(content).to match('BACKUP=NETFS')
-        expect(content).to match('BACKUP_URL=nfs://192.168.1.20/srv/rear/domain')
-        expect(content).to match('SSH_ROOT_PASSWORD=Recover!1234')
-      end
-
-      it 'should generate valid content for cron file rear' do
-        content = catalogue.resource('file', '/etc/cron.d/rear').send(:parameters)[:content]
-        expect(content).to match('0 2 \* \* 0 root /usr/sbin/rear mkbackup')
-      end
-
-      case facts[:operatingsystem]
-      when 'RedHat'
-      when 'OracleLinux'
-      when 'CentOS'
-      when 'Scientific'
-      end
-
       case facts[:osfamily]
       when 'RedHat'
-        if facts[:operatingsystemmajrelease] != '7'
-          it { is_expected.to contain_warning('The current operating system is not supported!') }
+        it { is_expected.to contain_class('rear::params') }
+        it { is_expected.to contain_class('rear::package') }
+        it { is_expected.to contain_class('rear::config') }
+        it { is_expected.to contain_class('rear::service') }
+
+        it { is_expected.to contain_package('rear').with_ensure('installed') }
+
+        it { is_expected.to contain_file('/etc/rear/local.conf').with_ensure('file') }
+
+        it { is_expected.to contain_file('/etc/cron.d/rear').with_ensure('file') }
+
+        it 'should generate valid content for configuration file local.config' do
+          content = catalogue.resource('file', '/etc/rear/local.conf').send(:parameters)[:content]
+          expect(content).to match('OUTPUT=ISO')
+          expect(content).to match('OUTPUT_URL=nfs://192.168.1.20/srv/rear/domain')
+          expect(content).to match('BACKUP=NETFS')
+          expect(content).to match('BACKUP_URL=nfs://192.168.1.20/srv/rear/domain')
+          expect(content).to match('SSH_ROOT_PASSWORD=Recover!1234')
+        end
+
+        it 'should generate valid content for cron file rear' do
+          content = catalogue.resource('file', '/etc/cron.d/rear').send(:parameters)[:content]
+          expect(content).to match('0 2 \* \* 0 root /usr/sbin/rear mkbackup')
         end
       else
         it { is_expected.to contain_warning('The current operating system is not supported!') }
